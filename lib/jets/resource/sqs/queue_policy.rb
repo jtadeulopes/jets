6a6476c438e919966505fe6686dc016a85c13e68
Jesus Lopes <jtadeulopes@gmail.com>
2020-05-29 18:00:31 -0300

    Remove merged_properties from QueuePolicy

--- lib/jets/resource/sqs/queue_policy.rb
+++ lib/jets/resource/sqs/queue_policy.rb
@@ -14,25 +14,6 @@ module Jets::Resource::Sqs
       }
     end
 
-    # Do not name this method properties, that is a computed method of `Jets::Resource::Base`
-    # def merged_properties
-    #   {
-    #     policy_document: {
-    #       version: "2012-10-17",
-    #       statement: {
-    #         effect: "Allow",
-    #         principal: { aws: "*"},
-    #         action: "sqs:SendMessage",
-    #         resource: "*",
-    #         # Condition:
-    #         #   ArnLike:
-    #         #     aws:SourceArn: arn:aws:s3:::aa-test-95872017
-    #       }
-    #     },
-    #     topics: ["!Ref {namespace}SnsTopic"],
-    #   }.deep_merge(@props)
-    # end
-
     def policy_logical_id
       "{namespace}_sqs_queue_policy"
     end
